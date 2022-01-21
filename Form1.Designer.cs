70e1be252ec081c8790d6b3136ebf2a9661dd732
Severstal-Metiz <severstal.2016@gmail.com>
2022-01-21 03:12:08 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,424 @@
+﻿namespace Kondey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tBC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tBR = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tBT = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tB3T = new System.Windows.Forms.TextBox();
+            this.tBTp = new System.Windows.Forms.TextBox();
+            this.tB3Tp = new System.Windows.Forms.TextBox();
+            this.tB5Tp = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tB5T = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.tBU = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.tBq = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.CustomProperties = "PointWidth=0.1";
+            series2.Legend = "Legend1";
+            series2.MarkerSize = 1;
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(717, 386);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 603);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(277, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Постоянный ток";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tBC
+            // 
+            this.tBC.Location = new System.Drawing.Point(130, 441);
+            this.tBC.Name = "tBC";
+            this.tBC.Size = new System.Drawing.Size(66, 20);
+            this.tBC.TabIndex = 2;
+            this.tBC.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 448);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ёмкость";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 448);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Мкф";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 474);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "МОм";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 474);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Сопротивление";
+            // 
+            // tBR
+            // 
+            this.tBR.Location = new System.Drawing.Point(130, 467);
+            this.tBR.Name = "tBR";
+            this.tBR.Size = new System.Drawing.Size(66, 20);
+            this.tBR.TabIndex = 5;
+            this.tBR.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(202, 500);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Сек";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 500);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "T=RC пост вр";
+            // 
+            // tBT
+            // 
+            this.tBT.Enabled = false;
+            this.tBT.Location = new System.Drawing.Point(130, 493);
+            this.tBT.Name = "tBT";
+            this.tBT.Size = new System.Drawing.Size(66, 20);
+            this.tBT.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(202, 526);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Сек";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(44, 526);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(20, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "3T";
+            // 
+            // tB3T
+            // 
+            this.tB3T.Enabled = false;
+            this.tB3T.Location = new System.Drawing.Point(130, 519);
+            this.tB3T.Name = "tB3T";
+            this.tB3T.Size = new System.Drawing.Size(66, 20);
+            this.tB3T.TabIndex = 11;
+            // 
+            // tBTp
+            // 
+            this.tBTp.Enabled = false;
+            this.tBTp.Location = new System.Drawing.Point(234, 493);
+            this.tBTp.Name = "tBTp";
+            this.tBTp.Size = new System.Drawing.Size(70, 20);
+            this.tBTp.TabIndex = 14;
+            this.tBTp.Text = "63%";
+            // 
+            // tB3Tp
+            // 
+            this.tB3Tp.Enabled = false;
+            this.tB3Tp.Location = new System.Drawing.Point(234, 519);
+            this.tB3Tp.Name = "tB3Tp";
+            this.tB3Tp.Size = new System.Drawing.Size(70, 20);
+            this.tB3Tp.TabIndex = 15;
+            this.tB3Tp.Text = "95%";
+            // 
+            // tB5Tp
+            // 
+            this.tB5Tp.Enabled = false;
+            this.tB5Tp.Location = new System.Drawing.Point(234, 545);
+            this.tB5Tp.Name = "tB5Tp";
+            this.tB5Tp.Size = new System.Drawing.Size(70, 20);
+            this.tB5Tp.TabIndex = 19;
+            this.tB5Tp.Text = "99%";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(202, 552);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(26, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Сек";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(44, 552);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(20, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "5T";
+            // 
+            // tB5T
+            // 
+            this.tB5T.Enabled = false;
+            this.tB5T.Location = new System.Drawing.Point(130, 545);
+            this.tB5T.Name = "tB5T";
+            this.tB5T.Size = new System.Drawing.Size(66, 20);
+            this.tB5T.TabIndex = 16;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(202, 421);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(14, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "В";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(44, 421);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(53, 13);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Напр. Up";
+            // 
+            // tBU
+            // 
+            this.tBU.Location = new System.Drawing.Point(130, 414);
+            this.tBU.Name = "tBU";
+            this.tBU.Size = new System.Drawing.Size(66, 20);
+            this.tBU.TabIndex = 20;
+            this.tBU.Text = "100";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(310, 500);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(14, 13);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "В";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(310, 526);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(14, 13);
+            this.label14.TabIndex = 24;
+            this.label14.Text = "В";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(310, 548);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(14, 13);
+            this.label15.TabIndex = 25;
+            this.label15.Text = "В";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(310, 581);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(20, 13);
+            this.label16.TabIndex = 28;
+            this.label16.Text = "Кл";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(44, 577);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(204, 13);
+            this.label17.TabIndex = 27;
+            this.label17.Text = "Конечный заряд q на конденсаторе 8T";
+            // 
+            // tBq
+            // 
+            this.tBq.Enabled = false;
+            this.tBq.Location = new System.Drawing.Point(254, 574);
+            this.tBq.Name = "tBq";
+            this.tBq.Size = new System.Drawing.Size(46, 20);
+            this.tBq.TabIndex = 26;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(348, 603);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(271, 46);
+            this.button2.TabIndex = 29;
+            this.button2.Text = "Синусоида";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 662);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.tBq);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.tBU);
+            this.Controls.Add(this.tB5Tp);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.tB5T);
+            this.Controls.Add(this.tB3Tp);
+            this.Controls.Add(this.tBTp);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tB3T);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tBT);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tBR);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tBC);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tBC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tBR;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tBT;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tB3T;
+        private System.Windows.Forms.TextBox tBTp;
+        private System.Windows.Forms.TextBox tB3Tp;
+        private System.Windows.Forms.TextBox tB5Tp;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tB5T;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox tBU;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.TextBox tBq;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kondey
+{
+    public partial class Form1 : Form
+    {
+
+        double C, R, Up, I, dt, dq,T,T3,T5,T8,Tp,T3p,T5p;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int Iteration = 1000;
+        double[] Uc;
+        double[] q;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Input();
+            Calc();
+            DrawGr();
+            Output();
+        }
+
+        void Input()
+        {
+            C = double.Parse(tBC.Text);  //значение ёмкости конденсатора
+            R = double.Parse(tBR.Text);  //значение резистора
+            Up = double.Parse(tBU.Text); //напряжение питания (в данном расчёте постоянное)
+        }
+
+        void Calc()
+        {
+            
+            T = R * C; //T - постоянная времени
+            T3 = T * 3;
+            T5 = T * 5;
+            T8 = T * 8;
+            dt = T8 / Iteration; //T8 максимальное время для расчёта Iteration - количество точек расчёта, чем больше тем точнее
+            Uc = new double[Iteration]; //напруга
+            q = new double[Iteration];  //заряды
+            Uc[0] = 0;
+            q[0] = 0;  
+            for (int i = 1; i < Iteration; i++)
+            {
+                I = (Up - Uc[i - 1]) / R;
+                dq = I * dt;
+                q[i] = q[i - 1] + dq;
+                Uc[i] = q[i] / C;
+                //черточки вертикальные
+                if (T / dt == (double)i) { Tp = Uc[i]; }
+                if (T3 / dt == (double)i) { T3p = Uc[i]; }
+                if (T5 / dt == (double)i) { T5p = Uc[i]; }
+            }
+
+
+        }
+
+        void Output()
+        {
+            tBT.Text = T.ToString();
+            tB3T.Text = T3.ToString();
+            tB5T.Text = T5.ToString();
+            tBTp.Text = string.Format("{0}%", Tp.ToString()) ;
+            tB3Tp.Text = string.Format("{0}%", T3p.ToString());
+            tB5Tp.Text = string.Format("{0}%", T5p.ToString());
+            tBq.Text = q[Iteration-1].ToString();
+        }
+
+        void DrawGr()
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            for (int i = 1; i < Iteration; i++)
+            {
+                chart1.Series[0].Points.AddXY( i * dt, Uc[i]);
+
+                if (Uc[i] == Tp) { chart1.Series[1].Points.AddXY(i * dt, Tp); }
+                if (Uc[i] == T3p) { chart1.Series[1].Points.AddXY(i * dt, T3p); }
+                if (Uc[i] == T5p) { chart1.Series[1].Points.AddXY(i * dt, T5p); }
+            }
+
+            
+
+        }
+
+
+    }
+}
